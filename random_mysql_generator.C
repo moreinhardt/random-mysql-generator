71a98f14054da683e9f5029e8be5ee7892d0b4b6
Moritz Reinhardt <iaminaustralia@gmail.com>
2009-11-28 06:00:14 +0100

    still a few bugs

--- random_mysql_generator.C
+++ random_mysql_generator.C
@@ -1,55 +1,66 @@
 #include<iostream>
 #include "statement.H"
 #include<vector>
+#include<cstdlib>
+#include<cstring>
 
 void usage(char * erromsg) {
 	std::cout << erromsg << std::endl;
 	std::cout << "USAGE: ./random-mysql-generator <tablename> <attr1> <type1> [<size1>] [<attr2> <type2> [<size2>]...] <number_of_entries>" << std::endl;
 	std::cout << "Where type = {integer, string}" << std::endl;
-	std::cout << "size only needed it type=string" << std::endl;
-	std::cout << "outputfile will be \"insert_<tablename>_<nrOfEntries>" << std::endl;
-	exit(1);
+	std::cout << "size only needed if type=string" << std::endl;
+	std::cout << "outputfile will be \"insert_<tablename>_<nrOfEntries>\"" << std::endl;
+	std::exit(1);
 }
 
 Statement * prepareArguments(int argc, char ** argv) {
-	char * tablename = argv[0];
-	vector<char*> attributes;
-	vector<char*> types;
-	vector<int> lengths;
-	for(int i=1; i<argc-1; ++i) {  //-1 because there are pairs // i=1 because it starts with the secand parameter
+	char * tablename = argv[1];
+	std::vector<char*> attributes;
+	std::vector<char*> types;
+	std::vector<int> lengths;
+	for(int i=2; i<argc-1; ++i) {  //-1 because there are pairs // i=1 because it starts with the secand parameter
 		attributes.push_back(argv[i++]); //i++ -> set i to type
-		switch(argv[i]) {
-			case "integer":
+		int s=0; //0=int
+		if(!(std::strcmp(argv[i],"string"))) //strcmp = 0 if the same
+			s=1;
+		
+		switch(s) {
+			case 0: //int
 				types.push_back("integer");
 				break;
-			case "string":
+			case 1: //string
 				types.push_back("string");
-				lengths.push_back(argv[++i]); //if string get length
+				lengths.push_back(atoi(argv[++i])); //if string get length
 				break;
 			default:
 				usage("Invalid parameters!");
 		}
 	}
-	Statement s = new Statement(tablename, attributes, types, lengths); //kopierkonstrutkor used...
+	Statement * s = new Statement(tablename, attributes, types, lengths); //kopierkonstrutkor used...
 	return s;
 }
 
+//TODO rethink!
 char * createFilename(char * tablename, char * nrOfEntries) {
 	int len = strlen(tablename);
 	len += strlen(nrOfEntries);
-	len += 7; //=strlen("insert_") + strlen("_")
-	char * filename = new char[len];
-	filename = "insert_" << tablename << "_" << nrOfEntries;
+	len += 8; //=strlen("insert_") + strlen("_")
+	char * filename = new char[len+1]; //+\0
+	strcpy(filename, "insert_");
+	strcat(filename, tablename);
+	strcat(filename, "_");
+	strcat(filename, nrOfEntries);
+	//filename = "insert_" << tablename << "_" << nrOfEntries;
 	return filename;
 }
 
 int main(int argc, char ** argv) {
 	if(argc < 4) usage("To few parameters!");
+	std::cout << static_cast<char>(50) << std::endl;
 	
 	Statement *s = prepareArguments(argc, argv);
-	char * filename = createFilename(argv[0], argv[argc-1]);
+	char * filename = createFilename(argv[1], argv[argc-1]);
 	s->createFile(filename, int(argv[argc-1])); //not sure if cast works with more than one digit
-
 	return 0;
 }
 
--- statement.C
+++ statement.C
@@ -1,30 +1,78 @@
 #include<iostream>
 #include<vector>
+#include<cstdlib>
+#include<string>
+#include<cstring>
+#include<cstdio> //sprintf()
 #include "statement.H"
 
 int Statement::getRandomInt(int max=0) {
 	srand( (unsigned) time(NULL) ) ;
-	if(!max)
+	if(!max) {
+		std::cout << "rand() = " << rand() << std::endl;
 		return rand();
-	else
+	} else
 		return rand() % (max+1);
 }
 
-char * Statement::getRandomString(int lenght) {
-	char *string = new char[lenth];  //memory leak
+char * Statement::getRandomString(int length) {
+	std::cout << "start getrandomstring()" << std::endl;
+	char *string = new char[length];  //memory leak
 	for(int i=0; i<length; ++i) {
-		string[i] = char(getRandomInt(127));	//127 max in asci?
+		string[i] = static_cast<char>(getRandomInt(126));	//127 max in asci?
 	}
 	return string;
 }
 
-Statement::Statement(char * tablename, vector<char*> attributes, vector<char*> types, vector<int> lengths) {
-	this->tablename = tablename;
+Statement::Statement(char * tablename, std::vector<char*> attributes, std::vector<char*> types, std::vector<int> lengths) {
+	this->table_name = tablename;
 	this->attributes = attributes;
 	this->types = types;
 	this->lengths = lengths;
 }
 
 bool Statement::createFile(char *filename, int count) {
+	std::string output("INSERT INTO ");
+	output += this->table_name;
+	output += " (";
+	int count_lengths = 0;
+	for(int i=0; i<this->attributes.size(); ++i) {
+		output += attributes[i];
+		output += ", ";
+	}
+	output.erase(output.end()-1); // cut ", " off last one
+	output.erase(output.end()-1);
 	
+	output += ") VALUES (";
+
+	//begin calculating random data and inserting it
+	for(int i=0; i<this->types.size(); ++i) {
+		int s=0; //0=int
+                if(!(std::strcmp(this->types[i],"string"))) //strcmp = 0 if the same
+                        s=1;
+
+                switch(s) {
+                        case 0: //int
+				char buffer[33];
+				sprintf(buffer,"%d",getRandomInt());
+				output += buffer;
+				output += ",";
+                                break;
+                        case 1: //string
+				output += "\'";
+				std::cout << "before string" << std::endl;
+				std::cout << "length: " << this->lengths[count_lengths] << std::endl;
+				output += getRandomString(this->lengths[count_lengths++]); //increas count afterwards
+				std::cout << "after" << std::endl;
+				output += "\',";
+                                break;
+                        default:
+				break;
+                }
+	}
+	output.erase(output.end()-1); //cut off ","
+	output += ");";
+	std::cout << output << std::endl;
+	
+	return true;	
 }
